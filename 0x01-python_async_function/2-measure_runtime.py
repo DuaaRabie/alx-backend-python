221f8c58769f48cfa4970822b38b0cb5479b3e4c
Duaa Rabie <DuaaRabie11@gmail.com>
2024-10-14 18:29:21 +0000

    update code

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Measure the runtime """
+
+
+import asyncio
+from typing import 
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, ma_delay: )
