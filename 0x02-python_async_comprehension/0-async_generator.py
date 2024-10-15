ce3951134a44c5c2fd4b471cc65187b7f9ae172e
Duaa Rabie <DuaaRabie11@gmail.com>
2024-10-15 09:39:15 +0000

    update code

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,5 +9,5 @@ import random
 async def async_generator():
     """ async generator """
     for _ in range(10):
-        await asyncio.sleep(1)
         yield random.random() * 10
+        await asyncio.sleep(1)
