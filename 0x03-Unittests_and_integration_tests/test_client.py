df1fd2316a6e8ecc076d0e974ba022a4b9de5d33
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-04 20:42:42 +0000

    task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" test client """
+
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ test that GithubOrgClient.org returns the correct value """
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('utils.get_json')
+    def test_org(self, org_name, mock_get_json):
+        # Set up the mock return value for get_json
+        mock_get_json.return_value = {"login": org_name}
+        # Create an instance of GithubOrgClient
+        client = GithubOrgClient(org_name)
+        # Call the org method
+        result = client.org()
+        # Ensure that get_json was called once with the correct URL
+        url = f'https://api.github.com/orgs/{org_name}'
+        mock_get_json.assert_called_once_with(url)
+        # Check that the returned value matches the expected result
+        self.assertEqual(result, {"login": org_name})
+
+
+if __name__ == '__main__':
+    unittest.main()
