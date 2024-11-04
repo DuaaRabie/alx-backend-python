117c4f0763362aa26f6842f6059d8e94e64f9378
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-04 20:49:04 +0000

    update code

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,18 +14,18 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",),
     ])
-    @patch('utils.get_json')
+    @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
-        # Set up the mock return value for get_json
+        """Set up the mock return value for get_json"""
         mock_get_json.return_value = {"login": org_name}
-        # Create an instance of GithubOrgClient
+        """Create an instance of GithubOrgClient"""
         client = GithubOrgClient(org_name)
-        # Call the org method
+        """Call the org method"""
         result = client.org()
-        # Ensure that get_json was called once with the correct URL
+        """ Ensure that get_json was called once with the correct URL """
         url = f'https://api.github.com/orgs/{org_name}'
         mock_get_json.assert_called_once_with(url)
-        # Check that the returned value matches the expected result
+        """Check that the returned value matches the expected result """
         self.assertEqual(result, {"login": org_name})
 
 
