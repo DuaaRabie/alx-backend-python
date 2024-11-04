5ad37f59ec015aeca7563183edf3fe3ea51f6c57
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-04 20:28:48 +0000

    update code

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,7 +19,6 @@ class TestAccessNestedMap(unittest.TestCase):
         """ method to test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
-
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
@@ -65,12 +64,14 @@ class TestMemoize(unittest.TestCase):
 
         test_instance = TestClass()
 
-        with patch.object(test_instance, 'a_method', return_value=42) as mock_method:
+        with patch.object(test_instance, 'a_method', return_value=42) \
+                as mock_method:
             result1 = test_instance.a_property
             result2 = test_instance.a_property
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
             mock_method.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
